a28192ced8363c2368070e5ba2c4cfc0e87af554
Ben McIntyre <ben@Bens-MBP.fios-router.home>
2023-07-24 13:42:18 -0700

    updates from nav words to icons

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="container mx-auto px-4 py-10">
+        <h1 className="text-white">Welcome</h1>
+    </div>
+  );
+}
+
+export {Dashboard};
