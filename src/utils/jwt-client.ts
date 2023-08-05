1367fd91e48c3385ca564c9e140312eab6d03d39
Myke Ahn-Stubbs <myke@mykestubbs.com>
2023-08-04 22:32:45 -0700

    Add JWTClient - JWT utilities

---
+++ src/utils/jwt-client.ts
@@ -0,0 +1,55 @@
+import jwt_decode from 'jwt-decode';
+
+interface IDecodedToken {
+  expires: number;
+  id: number;
+  email: string;
+}
+
+class JWTClient {
+  private tokenName: string;
+
+  constructor(tokenName: string = 'jwt_token') {
+    this.tokenName = tokenName;
+  }
+
+  public setToken(token: string): void {
+    const decodedToken:IDecodedToken = jwt_decode(token);
+    const expirationDate = new Date(decodedToken.expires).getTime();
+
+    // Store the token in the local storage
+    localStorage.setItem(this.tokenName, JSON.stringify({
+      token,
+      expirationDate,
+    }));
+  }
+
+  public getToken(): string | null {
+    const tokenObject = JSON.parse(localStorage.getItem(this.tokenName) || '{}');
+
+    // If the token exists, return it
+    if (tokenObject.token) { 
+      return tokenObject.token;
+    }
+
+    return null;
+  }
+
+  public removeToken(): void {
+    localStorage.removeItem(this.tokenName);
+  }
+
+  public decodeToken(): IDecodedToken | null {
+    const token = this.getToken();
+
+    if (token) {
+      return jwt_decode(token);
+    }
+
+    return null;
+  }
+}
+
+export {
+  JWTClient,
+}
